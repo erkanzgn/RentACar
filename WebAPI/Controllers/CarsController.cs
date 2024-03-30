0209a0ad5b5588e1cd495eb08bcc13b5d5b76115
Erkan özgün <eozgun65@gmail.com>
2024-03-30 22:49:21 +0300

    WebAPI ekledim

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = _carService.GetAll();
+            if (result.Success)
+            {
+                Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
